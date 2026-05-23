f670159128a17868bf6d1cd6998a2fbc7b3ea098
dianayule <caroyule@hotmail.com>
2026-05-23 15:18:58 +0200

    LAb solvedv2

--- sakila_labsolution.sql
+++ sakila_labsolution.sql
@@ -7,7 +7,7 @@ SHOW TABLES FROM sakila;
 -- Retrieve all the data from the tables actor, film and customer.
 SELECT * FROM sakila.actor;
 SELECT * FROM sakila.film;
-SELECT * FROM sakila.film;
+
 
 -- 3.Retrieve he following columns from their respective tables:
 -- 3.1 Titles of all films from the film table
@@ -17,7 +17,7 @@ SELECT name as language FROM sakila.language;
 
 -- 3.3 List of first names of all employees from the staff table
 SELECT first_name FROM sakila.staff;
--- Mike and Jon
+
 
 -- 4. Retrieve unique release years.
 SELECT  DISTINCT release_year FROM sakila.film;
@@ -33,15 +33,19 @@ SELECT COUNT(staff_id) AS Num_employees FROM sakila.staff;
 -- only 2
 
 -- 5.3 Determine how many films are available for rent and how many have been rented.
--- Number of files rented=183
+-- Number of files rented thus not available=183
 SELECT COUNT(inventory_id)
 FROM sakila.rental
 WHERE return_date IS NULL;
 
--- Number of files available =15861
-SELECT COUNT(inventory_id)
+-- Number of files that have been rented, same film rented more than once counts as one file rented ever =4580
+SELECT COUNT(DISTINCT inventory_id)
 FROM sakila.rental
 WHERE return_date IS NOT NULL;
+
+-- files registered in inventory = 4581 Thus files available are 4581-183= 4398
+SELECT COUNT(inventory_id)
+FROM sakila.inventory;
 
 -- 5.4 Determine the number of distinct last names of the actors in the database.
 SELECT COUNT(DISTINCT last_name) FROM sakila.actor;
